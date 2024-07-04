01eb9c25500e31d37e5bb65ee94f9d4cc78b71d5
nguyenthephung <136545799+nguyenthephung@users.noreply.github.com>
2024-07-04 11:54:34 +0700

    ..

--- Newpaper/Frontend/src/Customer/home/mainContent/popular/Popular.jsx
+++ Newpaper/Frontend/src/Customer/home/mainContent/popular/Popular.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "react-router-dom"
 import "./Popular.css"
 
 import Slider from "react-slick"
@@ -11,6 +12,7 @@ const Popular = () => {
   const settings = {
     className: "center",
     centerMode: false,
+    dots: true,
     infinite: true,
     centerPadding: "0",
     slidesToShow: 2,
@@ -36,24 +38,26 @@ const Popular = () => {
           <Slider {...settings}>
             {popular.map((val) => {
               return (
-                <div className='items'>
+                <div className='items' key={val.id}>
                   <div className='box shadow'>
                     <div className='images row'>
                       <div className='img'>
                         <img src={val.cover} alt='' />
                       </div>
-                      <div class='category category1'>
+                      <div className='category category1'>
                         <span>{val.catgeory}</span>
                       </div>
                     </div>
                     <div className='text row'>
-                      <h1 className='title'>{val.title.slice(0, 40)}...</h1>
+                      <Link to={`/popular/${val.id}`}>
+                        <h1 className='title'>{val.title.slice(0, 40)}...</h1>
+                      </Link>
                       <div className='date'>
-                        <i class='fas fa-calendar-days'></i>
+                        <i className='fas fa-calendar-days'></i>
                         <label>{val.date}</label>
                       </div>
                       <div className='comment'>
-                        <i class='fas fa-comments'></i>
+                        <i className='fas fa-comments'></i>
                         <label>{val.comments}</label>
                       </div>
                     </div>
